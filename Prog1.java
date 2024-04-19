e4aa8b41c34e2e52e1f2a39eacad7cbd5b06f843
Joan Obediente <joanobediente142001@gmail.com>
2024-04-19 01:37:15 -1200

    added three lines for ptc

--- Prog1.java
+++ Prog1.java
@@ -1,5 +1,7 @@
 public  class Prog1{
 public static void main(String[] args) {
-System.out.print("Pateros Technological College");
+System.out.print("Pateros");
+System.out.print("Technological College");
+System.out.print("College");
 }
 }
