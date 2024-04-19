d1ee27d5e2523cbb62aca9ac21ecbddbcd79b1cd
Joan Obediente <joanobediente142001@gmail.com>
2024-04-19 01:49:22 -1200

    added course to the code

--- Prog1.java
+++ Prog1.java
@@ -3,5 +3,6 @@ public static void main(String[] args) {
 System.out.print("Pateros");
 System.out.print("Technological College");
 System.out.print("College");
+System.out.print("Course: BSCS");
 }
 }
